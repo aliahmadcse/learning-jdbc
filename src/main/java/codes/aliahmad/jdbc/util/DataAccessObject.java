e06c32e795019c3b001368a23f68e2e07118274d
Ali Ahmad <aliahmadcse@gmail.com>
2021-10-18 22:23:16 +0500

    adds dao and dto's

---
+++ src/main/java/codes/aliahmad/jdbc/util/DataAccessObject.java
@@ -0,0 +1,51 @@
+package codes.aliahmad.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public abstract class DataAccessObject<T extends DataTransferObject>
+{
+  protected final Connection connection;
+  protected final static String LAST_VAL = "SELECT last_value FROM ";
+  protected final static String CUSTOMER_SEQUENCE = "hp_customer_seq";
+
+  private DataAccessObject(Connection connection)
+  {
+    super();
+    this.connection = connection;
+  }
+
+  public abstract T findById(long id);
+
+  public abstract List<T> findAll();
+
+  public abstract T update(T dataTransferObject);
+
+  public abstract T create(T dataTransferObject);
+
+  public abstract void delete(long id);
+
+  protected int getLastValue(String sequence)
+  {
+    int key = 0;
+    String sql = LAST_VAL + sequence;
+    try (Statement statement = connection.createStatement())
+    {
+      ResultSet resultSet = statement.executeQuery(sql);
+      while (resultSet.next())
+      {
+        key = resultSet.getInt(1);
+      }
+      return key;
+    }
+    catch (SQLException sqlException)
+    {
+      sqlException.printStackTrace();
+      throw new RuntimeException(sqlException);
+    }
+  }
+
+}
---
+++ src/main/java/codes/aliahmad/jdbc/util/DataTransferObject.java
@@ -0,0 +1,6 @@
+package codes.aliahmad.jdbc.util;
+
+public interface DataTransferObject
+{
+  long getId();
+}
